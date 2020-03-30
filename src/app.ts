f3c0f742b9ff593c96fb1fa86d4abc33c4dbf17c
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-03-30 16:02:19 -0600

    Arrows functions and implicits returns

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Ahoy! TypeScript!');
+const pizzas = [{ name: 'Pepperonni', toppings: ['pepperoni']}];
+
+const mappedPizzas= pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+function createPizza() {
+
+}
+
+const pizza = {
+	name: 'Blazing Inferno',
+	getName: function () {
+		console.log(this.name);
+	},
+};
+
+console.log(pizza.getName());
