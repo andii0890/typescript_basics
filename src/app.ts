f6a915bfc8d446a19ca7bd87f612e2c7495ac34c
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-04-01 13:44:04 -0600

    Function and optional arguments

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
-let  sumOrder = (price: number, quantity: number): number => {
-	return price * quantity;
+let  sumOrder: (price: number, quantity?: number) => number;
+
+sumOrder = (x, y) => {
+	if(y) {
+		return x * y;
+	}
+	return x; 
 }
 
-const sum = sumOrder(25, 2);
+const sum = sumOrder(25);
 
 console.log(`Total sum ${sum}`)
+
+
+// for an optional parameter add ?
