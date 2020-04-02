d05bfaca7586f90a360496198f1a9c1adda0e434
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-04-02 13:05:51 -0600

    Protected members and inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 abstract class Sizes {
-	constructor(public sizes: string[]) {}
+	constructor(protected sizes: string[]) {}
 
 	//read
 	set availableSizes(sizes: string[]) {
@@ -14,18 +14,26 @@ abstract class Sizes {
 class Pizza extends Sizes {
 	public toppings: string[] = []
 
-	constructor(readonly name: string, public sizes: string[]){
+	constructor(readonly name: string,  sizes: string[]){
 		super(sizes);
 	}
 
 	public addTopping(topping: string){
 		this.toppings.push(topping);
 	}
+
+	public updateSizes(sizes: string[]) {
+		this.sizes = sizes;
+	}
 }
 
 
 const pizza = new Pizza('pepperoni', ['small', 'medium']);
+
 console.log(pizza.availableSizes)
 
-pizza.addTopping('pepperoni');
+pizza.updateSizes(['large']);
+
+console.log(pizza.availableSizes)
+
 
